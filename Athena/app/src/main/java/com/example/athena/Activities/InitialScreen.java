7d52da1bcccfbb546fa67cdbe41dde6203ab1886
Ahmed <ashittu38@gmail.com>
2024-10-25 20:20:04 -0600

    1

--- Athena/app/src/main/java/com/example/athena/Activities/InitialScreen.java
+++ Athena/app/src/main/java/com/example/athena/Activities/InitialScreen.java
@@ -7,34 +7,47 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.athena.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * This is the first screen in the application that every user encounters upon their first usage.
  */
+
 public class InitialScreen extends AppCompatActivity {
 
+
+    private FirebaseFirestore Db;
+    ImageButton CheckCurrentEventsButton;
+    ImageButton NotificationsButton;
+    ImageButton ProfilePictureButton;
+    ImageButton ScanQRCodeButton;
+    ImageButton CreateEventButton;
+    ImageButton MoreOptionsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_screen);
 
         /// Assigns Button used for checking currently registered events
-        final ImageButton CheckCurrentEventsButton = findViewById(R.id.check_events_button);
+        ImageButton CheckCurrentEventsButton = findViewById(R.id.check_events_button);
 
         /// Assigns button used for checking notifications
-        final ImageButton NotificationsButton = findViewById(R.id.check_updates_button);
+        ImageButton NotificationsButton = findViewById(R.id.check_updates_button);
 
         ///Assigns button used for checking notifications
-        final ImageButton ProfilePictureButton =  findViewById(R.id.profile_picture_button);
+        ImageButton ProfilePictureButton =  findViewById(R.id.profile_picture_button);
 
         ///Assigns button used for checking notifications
-        final ImageButton ScanQRCodeButton = findViewById(R.id.scan_qr_code_button);
+        ImageButton ScanQRCodeButton = findViewById(R.id.scan_qr_code_button);
 
         ///Assigns button used for checking notifications
-        final ImageButton CreateEventButton = findViewById(R.id.create_event_button);
+        ImageButton CreateEventButton = findViewById(R.id.create_event_button);
 
         ///Assigns button used using additional features
-        final ImageButton MoreOptionsButton = findViewById(R.id.more_options_button);
+        ImageButton MoreOptionsButton = findViewById(R.id.more_options_button);
 
         ///Click listener for the check current events button
         CheckCurrentEventsButton.setOnClickListener(new View.OnClickListener() {
@@ -80,10 +93,20 @@ public class InitialScreen extends AppCompatActivity {
         });
 
 
-
-
+        MoreOptionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //WILL SWITCH TO THE DESIGNATED PAGE FOR THE USER'S SPECIFIC ROLE
+            }
+        });
 
 
 
     }
+
+
+
+
+
 }
+
