187bf7f84a7b09fa7a8814cebb6321342b6d512c
Ahmed <ashittu38@gmail.com>
2024-10-25 23:59:47 -0600

    Added User Navigation Logic for the UI, added some documentation, added a Signin-Signup Screen
    - the database connection isn't working we we're gonna need to resolve that, it may be an issue with the build of the project

---
+++ Athena/app/src/main/java/com/example/athena/Activities/signinScreenActivity.java
@@ -0,0 +1,72 @@
+package com.example.athena.Activities;
+
+import static android.widget.Toast.makeText;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.athena.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This class represent the class that the user will user to register their details or sign into the application
+ */
+public class signinScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_screen);
+
+        // Get Device ID
+        //Line derived from copilot: https://copilot.microsoft.com/?msockid=1ce35a3ee9c767ff0e974f22e87366e5, 2024-10-25
+        String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+
+        // Put Device ID in Bundle
+        Bundle bundle = new Bundle();
+        bundle.putString("device_id", deviceId);
+
+
+
+        ImageButton signinbutton = findViewById(R.id.signin_button);
+        ImageButton signupbutton = findViewById(R.id.register_button);
+
+        signinbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    switchToCorrespondingActivity(bundle);
+                }
+        });
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToCorrespondingActivity(bundle);
+            }
+        });
+
+
+
+    }
+
+    public void switchToCorrespondingActivity(Bundle deviceID) {
+
+        boolean is_admin = false;
+        //Place holder until the database is working, to test
+        if (!is_admin) {
+            Intent entrantsAndOrganizersHome = new Intent(this, entrantAndOrganizerHomeActivity.class);
+            entrantsAndOrganizersHome.putExtras(deviceID);
+            startActivity(entrantsAndOrganizersHome);
+        } else {
+            ////Just to represent what will happen when this is working
+            Toast admin = Toast.makeText(this, "This user is an Admin, he will get special privileges", Toast.LENGTH_SHORT);
+            admin.show();
+        }
+    }
+}
+
+
