ac31b9311d97a4e3749e109cedf2c696adf7e668
roger-chau <54104821+roger-chau@users.noreply.github.com>
2024-10-27 03:00:19 -0600

    added getters and setters

--- Athena/app/src/main/java/com/example/athena/Event/Event.java
+++ Athena/app/src/main/java/com/example/athena/Event/Event.java
@@ -36,11 +36,74 @@ public class Event {
             this.eventDate = eventDate;
         }
 
-    public void getEventName() {
-
+    public String getEventName() {
+        return eventName;
     }
 
-    public void setEventName() {
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFacilityID() {
+        return facilityID;
+    }
+
+    public void setFacilityID(String facilityID) {
+        this.facilityID = facilityID;
+    }
+
+    public Boolean getGeoRequire() {
+        return geoRequire;
+    }
+
+    public void setGeoRequire(Boolean geoRequire) {
+        this.geoRequire = geoRequire;
+    }
+
+    public Integer getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public void setMaxParticipants(Integer maxParticipants) {
+        this.maxParticipants = maxParticipants;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public SimpleDateFormat getRegStart() {
+        return regStart;
+    }
+
+    public void setRegStart(SimpleDateFormat regStart) {
+        this.regStart = regStart;
+    }
+
+    public SimpleDateFormat getRegEnd() {
+        return regEnd;
+    }
+
+    public void setRegEnd(SimpleDateFormat regEnd) {
+        this.regEnd = regEnd;
+    }
+
+    public SimpleDateFormat getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(SimpleDateFormat eventDate) {
+        this.eventDate = eventDate;
     }
 }
