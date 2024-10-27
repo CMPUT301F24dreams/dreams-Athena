f3d612b2e03845e688d7dc8157de7bc86b14f605
roger-chau <54104821+roger-chau@users.noreply.github.com>
2024-10-27 04:48:50 -0600

    updated gradle

--- Athena/app/build.gradle.kts
+++ Athena/app/build.gradle.kts
@@ -35,11 +35,13 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
---
+++ Athena/app/src/test/java/com/example/athena/EventTest.java
@@ -0,0 +1,4 @@
+package com.example.athena;
+
+public class EventTest {
+}
