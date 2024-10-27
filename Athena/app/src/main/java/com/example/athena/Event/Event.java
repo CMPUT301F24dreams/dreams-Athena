7791d45a12c0e4da0b312ece8d7cdd385343b786
Ahmed <ashittu38@gmail.com>
2024-10-27 16:29:22 -0600

    added more UI

--- Athena/app/src/main/java/com/example/athena/Event/Event.java
+++ Athena/app/src/main/java/com/example/athena/Event/Event.java
@@ -60,6 +60,7 @@ public class Event implements Model { // TO-DO Java-doc
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("Events").add(this);
     }
+    /*
     @Override
     public void addObserver(Observer observer) {
         observers.add(observer);
@@ -75,6 +76,8 @@ public class Event implements Model { // TO-DO Java-doc
             observer.update(this);
         }
     }
+    */
+
 
     // TO-DO: Add getters/setters properly
     public String getEventName() {
--- Athena/app/src/main/java/com/example/athena/WaitList/WaitList.java
+++ Athena/app/src/main/java/com/example/athena/WaitList/WaitList.java
@@ -1,32 +1,75 @@
 package com.example.athena.WaitList;
 
+import com.example.athena.Event.Event;
 import com.example.athena.Roles.User;
+
 
 import java.util.ArrayList;
 
+public class WaitList{
+    private ArrayList<User> waiting;
+    private ArrayList<User> invited;
+    private ArrayList<User> declined;
+    private Event attachedEvent;
 
-/**
- * This class represents waitlists within the application
- */
-public class WaitList {
-    private ArrayList <User> Waitlist;
-
-    public WaitList(){
-       this.Waitlist = new ArrayList<User>();
-   }
-    public void setWaitlist(ArrayList <User> waitlist){
-            this.Waitlist = waitlist;
-   }
-
-    public ArrayList<User> getWaitlist() {
-        return Waitlist;
+    public WaitList(Event event){
+        waiting = new ArrayList<User>();
+        invited = new ArrayList<User>();
+        declined = new ArrayList<User>();
+        attachedEvent = event;
     }
 
-    public void add(User newEntrant){
-       Waitlist.add(newEntrant);
+    public ArrayList<User> getDeclined() {
+        return declined;
+    }
+
+    public Event getAttachedEvent() {
+        return attachedEvent;
+    }
+
+    public ArrayList<User> getWaiting() {
+        return waiting;
+    }
+
+    public ArrayList<User> getInvited() {
+        return invited;
+    }
+
+    public void addWaiting(User user) {
+        this.waiting.add(user);
+    }
+
+
+    /**
+     * Randomly selects x amount of users to be sent a invitation to the event
+     * @param numSelect the number of users to be selected
+     */
+    public void selectUsersToInvite(int numSelect){
+
+        if (numSelect > waiting.size()){
+            //Throw an exception if numSelected is greater than the amount of users waiting
+            try {
+                throw new Exception("Number selected is greater than amount of users signed up");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        //get random from list and move it
+        for (int i = 0; i < numSelect; i++) {
+            int indexNum = (int) (Math.random() * waiting.size());
+            moveUsers(waiting.get(indexNum),invited,waiting);
+        }
 
     }
-//CREATE METHODS FOR THE WAITLIST
 
-
+    /**
+     * Moves user object from one array list to another
+     * @param user user object to be moved
+     * @param receiver array list that adds the user
+     * @param donor array list that removes the user
+     */
+    private void moveUsers(User user, ArrayList<User> receiver, ArrayList<User> donor){
+        receiver.add(user);
+        donor.remove(user);
+    }
 }
