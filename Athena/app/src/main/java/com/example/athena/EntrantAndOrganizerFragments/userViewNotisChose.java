4962592c482320698813478775051f4d82722537
Ahmed <ashittu38@gmail.com>
2024-11-05 23:02:02 -0700

    slight fixes to UI

--- Athena/app/src/main/java/com/example/athena/EntrantAndOrganizerFragments/userViewNotisChose.java
+++ Athena/app/src/main/java/com/example/athena/EntrantAndOrganizerFragments/userViewNotisChose.java
@@ -2,18 +2,15 @@ package com.example.athena.EntrantAndOrganizerFragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-import com.example.athena.Event.Event;
 import com.example.athena.R;
 
 public class userViewNotisChose extends DialogFragment {
