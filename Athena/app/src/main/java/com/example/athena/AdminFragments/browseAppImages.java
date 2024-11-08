7740cb13d4ee20a828f4b2df3affdb1a250e5521
Ahmed <ashittu38@gmail.com>
2024-11-08 00:07:57 -0700

    Added more pages for the admin functionalities

---
+++ Athena/app/src/main/java/com/example/athena/AdminFragments/browseAppImages.java
@@ -0,0 +1,28 @@
+package com.example.athena.AdminFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.athena.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link browseAppImages#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class browseAppImages extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.admin_view_images, container, false);
+    }
+}
