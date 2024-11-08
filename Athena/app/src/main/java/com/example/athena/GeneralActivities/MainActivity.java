8bee6f7c629e4ac29b8849563fb380a9237888fb
scarson5103 <128325532+scarson5103@users.noreply.github.com>
2024-11-08 16:36:44 -0700

    uncomment notification service

--- Athena/app/src/main/java/com/example/athena/GeneralActivities/MainActivity.java
+++ Athena/app/src/main/java/com/example/athena/GeneralActivities/MainActivity.java
@@ -60,15 +60,13 @@ public class MainActivity extends AppCompatActivity {
                         transaction.replace(R.id.content_layout, signUp); // Replace with your container ID
                         transaction.commit();
                     }
-                    /*
+                    
                     // Notification setup
                     requestNotificationPermission();
 
                     Intent notificationIntent = new Intent(MainActivity.this, NotificationService.class);
 
                     startService(notificationIntent);
-
-                     */
                 }
             });
         }
