1797baaab0d8a18ade2913754f5189a6bda4490c
Ahmed <ashittu38@gmail.com>
2024-11-08 01:30:47 -0700

    More UI Adjustments

--- Athena/app/src/main/java/com/example/athena/AdminFragments/browseAppImages.java
+++ Athena/app/src/main/java/com/example/athena/AdminFragments/browseAppImages.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.athena.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link browseAppImages#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class browseAppImages extends Fragment {
 
 
